8c08c814aba17396bacae07108091f6395ba4a41
@NikitaGautam09 <nikita.sp19@nsut.ac.in>
2022-07-06 21:58:10 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function Header() {
+
+
+    return(
+
+
+        <nav class="bg-dark navbar-dark navbar">
+
+
+            <div className="row col-12 d-flex justify-content-center text-white">
+
+
+                <h3>Google Brain Teaser</h3>
+
+
+            </div>
+
+
+        </nav>
+
+
+    )
+
+
+}
+
+
+export default Header;
